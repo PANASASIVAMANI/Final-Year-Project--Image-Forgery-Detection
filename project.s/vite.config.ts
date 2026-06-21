83abe04866e7763757f42a2d4a617425cce3c450
PANASASIVAMANI <panasasivamani9090@gmail.com>
2026-06-21 22:59:29 +0530

    Configure GitHub Pages deployment and resolve conflicts

--- project.s/vite.config.ts
+++ project.s/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Final-Year-Project--Image-Forgery-Detection/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
